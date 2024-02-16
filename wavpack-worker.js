056fb0aa92c7ad12625124dbaf09cea951a33864
Administrator <admin@MacBook-Pro.local>
2024-02-17 00:12:43 +0800

    2024021608

--- wavpack-worker.js
+++ wavpack-worker.js
@@ -80,7 +80,7 @@ const play = (wvData) => {
 
     sample_rate = Module.ccall('GetSampleRate', null, [], []);
     if (sample_rate <= 64000) {
-        fetching_interval = 20;
+        fetching_interval = 32;
     }
     else {
         fetching_interval = 8;
